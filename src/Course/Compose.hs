e2ad5d5078fee1f09e8c91aa289c9f540e1427cb
jecaro <jeancharles.quillet@gmail.com>
2020-08-12 14:36:46 +0200

    Solve the Compose module

--- src/Course/Compose.hs
+++ src/Course/Compose.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE InstanceSigs #-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
@@ -17,17 +18,20 @@ newtype Compose f g a =
 -- Implement a Functor instance for Compose
 instance (Functor f, Functor g) =>
     Functor (Compose f g) where
-  (<$>) =
-    error "todo: Course.Compose (<$>)#instance (Compose f g)"
+  (<$>) :: (a -> b) -> Compose f g a -> Compose f g b
+  (<$>) fab (Compose (f :: f (g a))) =
+     Compose $ ((<$>) . (<$>)) fab f
 
 instance (Applicative f, Applicative g) =>
   Applicative (Compose f g) where
 -- Implement the pure function for an Applicative instance for Compose
+  pure :: a -> Compose f g a
   pure =
-    error "todo: Course.Compose pure#instance (Compose f g)"
+    Compose . pure . pure
 -- Implement the (<*>) function for an Applicative instance for Compose
-  (<*>) =
-    error "todo: Course.Compose (<*>)#instance (Compose f g)"
+  (<*>) :: Compose f g (a -> b) -> Compose f g a -> Compose f g b
+  (<*>) (Compose (fgab :: f (g (a -> b)))) (Compose (fga :: f (g a))) =
+     Compose $ flip (<*>) <$> fga <*> fgab
 
 instance (Monad f, Monad g) =>
   Monad (Compose f g) where
@@ -41,5 +45,6 @@ instance (Monad f, Monad g) =>
 instance (Functor f, Contravariant g) =>
   Contravariant (Compose f g) where
 -- Implement the (>$<) function for a Contravariant instance for Compose
-  (>$<) =
-    error "todo: Course.Compose (>$<)#instance (Compose f g)"
+  (>$<) :: (b -> a) -> Compose f g a -> Compose f g b
+  (>$<) fba (Compose (fga :: f (g a))) =
+     Compose $ ((<$>) . (>$<)) fba fga
